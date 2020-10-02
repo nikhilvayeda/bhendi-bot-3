81641cda06f8dfeb92cefff0be8086cdef7b2cf1
chief141 <prashantrahul23@gmail.com>
2020-10-02 18:51:26 +0530

    adding basic structure and load , unload command, reload command

---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+import sys
+import discord
+from discord.ext import commands
+import Constants as consts
+
+
+# Instances
+client = commands.Bot(command_prefix=consts.PREFIX)
+client.remove_command("help")
+
+
+@client.event
+async def on_ready():
+    print("[Bhendi Bot 3.0] Ready...")
+
+
+# To load cogs
+@client.command()
+async def load(ctx, extension=None):
+    if int(ctx.author.id) in consts.DEVS_IDS and extension is not None:
+        if str(extension) in consts.ALL_EXTENSIONS:
+
+            try:
+                client.load_extension(f"cogs.{extension}")
+                print(f"[Bhendi Bot 3.0] Loaded : {extension}")
+                await ctx.send(f"Loaded the {extension} Cog")
+
+            except Exception as e:
+                print(f"[Bhendi Bot 3.0] Failed to load : {extension}")
+                print(f"[Bhendi Bot 3.0] ERROR : {e}")
+
+
+# To unload cogs
+@client.command()
+async def unload(ctx, extension=None):
+    if int(ctx.author.id) in consts.DEVS_IDS and extension is not None:
+        if str(extension) in consts.ALL_EXTENSIONS:
+
+            try:
+                client.unload_extension(f"cogs.{extension}")
+                print(f"[Bhendi Bot 3.0] Unloaded : {extension}")
+                await ctx.send(f"Unloaded the {extension} Cog")
+
+            except Exception as e:
+                print(f"[Bhendi Bot 3.0] Failed to unload : {extension}")
+                print(f"[Bhendi Bot 3.0] ERROR : {e}")
+
+
+# To reload cogs
+async def reload(ctx, extension=None):
+    await load(ctx, extension)
+    await unload(ctx, extension)
+
+
+# Loading all the cogs in the starting
+for filename in os.listdir(os.path.join(os.getcwd(), "cogs")):
+    if filename.endswith(".py") and filename[:-3] in consts.ALL_EXTENSIONS:
+        client.load_extension(f"cogs.{filename[:-3]}")
+
+
+# Running
+client.run(consts.BOT_TOKEN)
