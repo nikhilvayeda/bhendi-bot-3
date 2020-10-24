000aa02234c996ec918278b44da037cef01bd023
chief141 <prashantrahul23@gmail.com>
2020-10-24 13:28:16 +0530

    adding github command

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import time
 import platform
+import requests
 
 class Utilities(commands.Cog):
     def __init__(self, client):
@@ -15,6 +16,8 @@ class Utilities(commands.Cog):
                     f"Architecture : **{platform.architecture()}**\n"\
                     f"Machine : **{platform.machine()}**\n"\
 
+        self.api_url = "https://api.github.com/repos/nikhilvayeda/bhendi-bot-3"
+
     @commands.command()
     async def ping(self, ctx):
         '''Shows the latency.'''
@@ -26,6 +29,23 @@ class Utilities(commands.Cog):
 
         _live_stats = f"**__Stats__**\n\nUptime : **{round(time.perf_counter() - self.started)}s**\nLatency: {round(self.client.latency * 1000)} ms\n"
         await ctx.send(_live_stats + self.stats)
+
+
+    @commands.command()
+    async def github(self, ctx):
+        '''Guthub status'''
+
+        _res = requests.get(self.api_url)
+
+        if _res.status_code == 200:
+            _json_data = _res.json()
+            _main_str = f"**Url** : {_json_data['html_url']}\n**Size** : {_json_data['size']}\n**Stars** : {_json_data['stargazers_count']}\n"\
+                    f"**Watchers** : {_json_data['watchers_count']}\n**Forks** : {_json_data['forks_count']}"
+
+            await ctx.send(_main_str)
+            return True
+
+        await ctx.send("Failed to retrieve data from github.")
 
 
 def setup(client):
